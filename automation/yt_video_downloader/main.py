d39b454972a67696bdc4cfbd67acf81179146eb9
PlutoPut <mic.plu02@gmail.com>
2024-03-03 00:54:23 +0100

    yt downloader

--- automation/yt_video_downloader/main.py
+++ automation/yt_video_downloader/main.py
@@ -1,9 +1,21 @@
-from pytube import YouTube
+from pytube import YouTube, exceptions
 from sys import argv
 
-link = argv[1]
+try:
+    link = argv[1]
+except IndexError:
+    try:
+        link = input("paste a video link here: ")
+        print("you must provide link to a video as second parameter")
+    except:
+        print("wrong input")
+        exit()
 
-yt = YouTube(link)
+try:
+    yt = YouTube(link)
+except exceptions.RegexMatchError:
+    print("Couldn't find this video, sorry")
+    exit()
 
 print("Title: ",yt.title)
 
